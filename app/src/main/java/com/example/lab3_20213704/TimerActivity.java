99998dd3ec968fbf960a9750998864e6033c075e
Hineill <hineill.cespedes@pucp.edu.pe>
2024-09-20 23:52:22 -0500

    Cerrar sesión

--- app/src/main/java/com/example/lab3_20213704/TimerActivity.java
+++ app/src/main/java/com/example/lab3_20213704/TimerActivity.java
@@ -1,12 +1,20 @@
 package com.example.lab3_20213704;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.sql.Time;
 
 public class TimerActivity extends AppCompatActivity {
 
@@ -15,5 +23,25 @@ public class TimerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.timer);
+
+//        Toolbar toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//
+//        toolbar.setTitle("PomodoroPUCP");
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.timer_menu, menu);
+        return true;
+    }
+
+    public void volverInicioSesion(MenuItem item ){
+        Intent intent = new Intent(TimerActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
+
+
 }
