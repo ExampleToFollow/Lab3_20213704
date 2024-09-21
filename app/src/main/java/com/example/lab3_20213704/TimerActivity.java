dd964a05072e17701c3f369c3204f78d183c5fb0
Hineill <hineill.cespedes@pucp.edu.pe>
2024-09-21 02:49:10 -0500

    Reloj funcionando

--- app/src/main/java/com/example/lab3_20213704/TimerActivity.java
+++ app/src/main/java/com/example/lab3_20213704/TimerActivity.java
@@ -1,6 +1,7 @@
 package com.example.lab3_20213704;
 
 import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.SystemClock;
@@ -15,6 +16,7 @@ import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.ActivityChooserView;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -23,6 +25,7 @@ import android.os.CountDownTimer;
 
 
 import com.example.lab3_20213704.ConsumoWebServices.Interfaces.Usuario;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 import java.sql.Time;
 
@@ -31,6 +34,11 @@ public class TimerActivity extends AppCompatActivity {
     private ImageView startButton;
     private CountDownTimer timer;
     private final long startTimeInMillis = 25 * 60 * 1000;
+
+    private final long startTimeBreakInMillis = 5 * 60 * 1000;
+
+    private String idUsuario;
+
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,7 +55,7 @@ public class TimerActivity extends AppCompatActivity {
         String firstName  = intent.getStringExtra("firstName");
         String lastName  = intent.getStringExtra("lastName");
         String email  = intent.getStringExtra("email");
-
+        idUsuario = idUser;
         TextView nameView = findViewById(R.id.name);
         nameView.setText(firstName + " " + lastName);
 
@@ -64,7 +72,6 @@ public class TimerActivity extends AppCompatActivity {
 
         timerTextView = findViewById(R.id.timer);
         startButton = findViewById(R.id.start_button);
-
         startButton.setOnClickListener(v -> startCountdown());
     }
 
@@ -81,11 +88,11 @@ public class TimerActivity extends AppCompatActivity {
     }
 
     private void startCountdown() {
-        // Cancela cualquier cronómetro anterior si existe
         if (timer != null) {
             timer.cancel();
         }
-
+        startButton.setImageResource(R.drawable.restart_alt_24px);
+        startButton.setOnClickListener(v -> restartCountdown());
         // Configura un nuevo cronómetro
         timer = new CountDownTimer(startTimeInMillis, 1000) {
             @Override
@@ -95,15 +102,102 @@ public class TimerActivity extends AppCompatActivity {
                 int seconds = (int) (millisUntilFinished / 1000) % 60;
                 timerTextView.setText(String.format("%02d:%02d", minutes, seconds));
             }
-
             @Override
             public void onFinish() {
                 // Acciones a realizar cuando el cronómetro finaliza
-                timerTextView.setText("00:00");
+                timerTextView.setText("05:00");
+                //Empieza el break o se decide que vaya a otra vista a marcar el spinner
+                startBreak();
             }
         }.start();
     }
 
+    private void restartCountdown() {
+        // Cancela cualquier cronómetro anterior si existe
+        if (timer != null) {
+            timer.cancel();
+        }
+        //Cambia el icono
+        startButton.setImageResource(R.drawable.restart_alt_24px);
+        startButton.setOnClickListener(v -> restartCountdown());
+        // Configura un nuevo cronómetro
+        timer = new CountDownTimer(startTimeInMillis, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                int minutes = (int) (millisUntilFinished / 1000) / 60;
+                int seconds = (int) (millisUntilFinished / 1000) % 60;
+                timerTextView.setText(String.format("%02d:%02d", minutes, seconds));
+            }
+            @Override
+            public void onFinish() {
+                timerTextView.setText("05:00");
+                startBreak();
+            }
+        }.start();
+    }
+
+
+    public void mostrarDialog(){
+        new MaterialAlertDialogBuilder(this)
+                .setTitle("!Felicidades¡")
+                .setMessage("Empezó el tiempo de descanso")
+                .setPositiveButton("Entendido", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss(); // Cierra el diálogo
+                    }
+                })
+                .show();
+    }
+
+
+    public void startBreak(){
+        boolean validarSinTareas =true ;
+
+        //Invocamos la c
+
+
+
+        if(validarSinTareas){
+            if (timer != null) {
+                timer.cancel();
+            }
+            //Cambia el icono
+            mostrarDialog();
+            startButton.setVisibility(View.INVISIBLE);
+            startButton.setClickable(false);
+            //startButton.setOnClickListener(v -> restarCountdown());
+            // Configura un nuevo cronómetro
+            timer = new CountDownTimer(startTimeBreakInMillis, 1000) {
+                @Override
+                public void onTick(long millisUntilFinished) {
+                    // Actualiza el texto del TextView con el tiempo restante
+                    int minutes = (int) (millisUntilFinished / 1000) / 60;
+                    int seconds = (int) (millisUntilFinished / 1000) % 60;
+                    timerTextView.setText(String.format("%02d:%02d", minutes, seconds));
+                }
+                @Override
+                public void onFinish() {
+                    // Acciones a realizar cuando el cronómetro finaliza
+                    timerTextView.setText("25:00");
+                    prepararInicio();
+                }
+            }.start();
+        }else{
+            //Caso tenga tareas pendientes irá al otro activity
+            Intent intent = new Intent(TimerActivity.this , FinalActivity.class);
+            startActivity(intent);
+        }
+    }
+
+
+    public void prepararInicio(){
+        timerTextView.setText("25:00");
+        startButton.setVisibility(View.VISIBLE);
+        startButton.setClickable(true);
+        startButton.setImageResource(R.drawable.resume_24px);
+        startButton.setOnClickListener(v -> startCountdown());
+    }
 
 
 
