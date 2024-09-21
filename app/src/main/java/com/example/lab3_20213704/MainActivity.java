c72f998558c01313a4c79fbd04f47f63e79e88f2
Hineill <hineill.cespedes@pucp.edu.pe>
2024-09-20 22:03:18 -0500

    Login validado 100%

--- app/src/main/java/com/example/lab3_20213704/MainActivity.java
+++ app/src/main/java/com/example/lab3_20213704/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.lab3_20213704;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.os.Bundle;
@@ -9,6 +10,7 @@ import android.view.View;
 import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.graphics.drawable.AnimatedStateListDrawableCompat;
 import androidx.core.graphics.Insets;
@@ -19,6 +21,7 @@ import com.example.lab3_20213704.ConsumoWebServices.Interfaces.AuthUsuario;
 import com.example.lab3_20213704.ConsumoWebServices.Interfaces.RespuestaAuth;
 import com.example.lab3_20213704.ConsumoWebServices.Interfaces.Usuario;
 import com.example.lab3_20213704.ConsumoWebServices.Interfaces.UsuarioService;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 import java.util.List;
 
@@ -48,16 +51,11 @@ public class MainActivity extends AppCompatActivity {
 
     //Llevará a la vista principal en caso se válide correctamente
     public void IniciarSesion(View view){
-
-
         AuthUsuario authUsuario = (AuthUsuario) new Retrofit.Builder()
                 .baseUrl("https://dummyjson.com")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
                 .create(AuthUsuario.class);
-
-
-        //Ahora las sacamos de los inputs
 
         String username = ((EditText) findViewById(R.id.userInput)).getText().toString();
         String password = ((EditText) findViewById(R.id.passwordInput)).getText().toString();
@@ -65,17 +63,32 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call<RespuestaAuth> call, Response<RespuestaAuth> response) {
                 //Pasar a la vista timer
-                Intent intent = new Intent(MainActivity.this, TimerActivity.class);
-                startActivity(intent);
+                if(response.isSuccessful()){
+                    Intent intent = new Intent(MainActivity.this, TimerActivity.class);
+                    startActivity(intent);
+                }else{
+                    showMaterialDialog();
+                }
             }
-
             @Override
             public void onFailure(Call<RespuestaAuth> call, Throwable t) {
                 Log.e("Error", "Ayuda");
             }
         });
+    }
 
 
-
+    private void showMaterialDialog() {
+        // Crea y configura el diálogo
+        new MaterialAlertDialogBuilder(this)
+                .setTitle("Credenciales incorrectas")
+                .setMessage("Ingreso mal sus credenciales")
+                .setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss(); // Cierra el diálogo
+                    }
+                })
+                .show();
     }
 }
